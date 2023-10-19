e79489c2ab00366494368aee98bdd2ee38ccb8d0
AngelHaziel <101890056+AngelHaziel@users.noreply.github.com>
2023-10-19 06:12:18 -0600

    Update recibir_usuario.php

--- recibir_usuario.php
+++ recibir_usuario.php
@@ -8,3 +8,31 @@ if ($userId > 0) {
     echo 'ID de usuario no válido.';
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lista de archivos PDF</title>
+</head>
+<body>
+    <h1>Archivos PDF en el Directorio</h1>
+    <ul>
+        <?php
+        // Directorio donde se encuentran los archivos PDF
+        $directorio = 'PDF/';
+
+        // Escanea el directorio para obtener una lista de archivos
+        $archivos = scandir($directorio);
+
+        // Filtra los archivos PDF
+        $archivos_pdf = array_filter($archivos, function($archivo) {
+            return pathinfo($archivo, PATHINFO_EXTENSION) === 'pdf';
+        });
+
+        // Muestra los enlaces a los archivos PDF en una lista
+        foreach ($archivos_pdf as $pdf) {
+            echo '<li><a href="' . $directorio . $pdf . '" target="_blank">' . $pdf . '</a></li>';
+        }
+        ?>
+    </ul>
+</body>
+</html>
